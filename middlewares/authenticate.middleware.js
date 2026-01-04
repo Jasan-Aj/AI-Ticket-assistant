7057c685767c98f8b13e1d12773230d9399b6784
Jasan-Aj <mjasan9@gmail.com>
2026-01-04 13:53:44 +0530

    created authenticator middleware

---
+++ middlewares/authenticate.middleware.js
@@ -0,0 +1,32 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+
+export const authenticate = async (req, res, next)=>{
+    try{
+        let token;
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token = req.headers.authorization.split(" ")[1]; 
+        }
+
+        if(!token){
+            throw new Error();
+        }
+
+        const decoded = jwt.decode(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.uderId);
+
+        if(!user){
+            throw new Error();
+        }
+
+        req.user = user;
+        next();
+        
+    }catch(error){
+        res.send({
+            success: false,
+            error: "Not authorized!"
+        });
+    }
+}
