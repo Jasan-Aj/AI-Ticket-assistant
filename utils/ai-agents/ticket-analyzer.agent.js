7a034b092bd90883baac13b28fbd4f859658248b
Jasan-Aj <mjasan9@gmail.com>
2026-01-05 22:38:32 +0530

    created prompt for model

--- utils/ai-agents/ticket-analyzer.agent.js
+++ utils/ai-agents/ticket-analyzer.agent.js
@@ -7,8 +7,48 @@ export const analyzeTicket = async (ticket)=>{
            apiKey: process.env.GEMINI_API_KEY 
         }),
         name: "AI Ticket Analytic Assistant",
-        system: ``,
+        system: `You are an expert AI assistant that processes technical support tickets.
+        
+        Your job is to:
+        1. Summarize the issue.
+        2. Estimate its priority.
+        3. Provide helpful notes and resources links for human moderators.
+        4. List relevant technical skills required.
+
+        IMPORTANT:
+        - Respond with *only* valid raw JSON.
+        -Do NOT include markdown, code fences, comments, or any extra formatting.
+        - The format must be a raw JSON object.
+
+        Repeat: Do not wrap your output in markdown or code fences.`,
     });
 
-    const response = await supportAgent.run('');
+    const response = await supportAgent.run(`You are a ticket triage agent Only return strict JSON object with no extra headers, or markdown.
+        
+        Analyze the following support tcket and provide a JSON object with:
+        
+        - summary: A short 1-2 sentence summary of the issue.
+        - priority: One of "low" "medium" "high".
+        - helpfulNotes: A detailed technical explination that a moderator can use to solve this issue. Include useful external links or resources if possible.
+        - relatedSkills: An array of relevant skills required to solve the issue (eg., ["React", "MongoDB"]).
+        
+        Respond ONLY in this JSON format and do not iclude any other text or markdown in the answer:
+
+        {
+        "summary": "Short summary of the ticket",
+        "priority": "high",
+        "helpfulNotes": "Here are useful tips...",
+        "relatedSkills": ["React", "Node.js"]
+        }
+
+        ---
+
+        Ticket information:
+
+        - Title: ${ticket.title}
+        - Description: ${ticket.description}`);
+
+    const raw = response.output[0].context;
+
+    
 }
