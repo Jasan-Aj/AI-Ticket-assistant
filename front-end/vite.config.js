3204bdbd8b577a6239ed2be1e346558af4611610
Jasan-Aj <mjasan9@gmail.com>
2026-02-14 00:19:11 +0530

    minor changes

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    '/api': 'https://ai-ticket-assistant-4qfw.vercel.app'
+  },
   plugins: [react(), tailwindcss()],
 })
