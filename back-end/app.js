626d94647c6223a58c9b93d065a0a7bb347a68c9
Jasan-Aj <mjasan9@gmail.com>
2026-02-13 18:11:45 +0530

    minor changes

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 import { connectDatabase } from "./database/mongodb.js";
 import cors from "cors";
 import userRoutes from "./routers/user.routes.js";
@@ -7,35 +7,35 @@ import ticketRoutes from "./routers/ticket.routes.js";
 import { serve } from "inngest/express";
 import { inngest } from "./inngest/client.js";
 import dotenv from "dotenv";
-import {onUserSignUp} from "./inngest/functions/on-sign-up.js";
-import {onTicketCreated} from "./inngest/functions/on-ticket-create.js"
+import { onUserSignUp } from "./inngest/functions/on-sign-up.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-dotenv.config();
+
+
+connectDatabase().catch(err => console.error("MongoDB connection error:", err));
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/ticket", ticketRoutes);
 
-app.use("/api/inngest",serve({
+
+app.use("/api/inngest", serve({
     client: inngest,
-    functions:[onTicketCreated, onUserSignUp]
+    functions: [onTicketCreated, onUserSignUp]
 }));
 
-const startServer = async ()=>{
-    try{
-        await connectDatabase();
-        app.listen(process.env.PORT,()=>{
-            console.log(`Server running on PORT: ${process.env.PORT || 3000}`);
-        })
-    }catch(error){
-        console.log(error);
-    }
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
 }
 
-
-
-startServer();
+export default app;
