23ad76db935f7a56df06b627d5a27cb0c07695c8
Jasan-Aj <mjasan9@gmail.com>
2026-02-13 23:14:23 +0530

    set up vercel.json file

--- back-end/app.js
+++ back-end/app.js
@@ -10,6 +10,10 @@ import dotenv from "dotenv";
 import { onUserSignUp } from "./inngest/functions/on-sign-up.js";
 import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
+dotenv.config();
+
+const app = express();
+
 app.use(cors({
   origin:'https://ai-ticket-assistant-4qfw.vercel.app',
   credentials: true,
@@ -17,11 +21,9 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'OPTIONS']
 }));
 
-dotenv.config();
-const app = express();
+app.options('*', cors());
 
 app.use(express.json());
-
 
 
 connectDatabase().catch(err => console.error("MongoDB connection error:", err));
