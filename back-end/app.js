8bf4a134bba4269a9e05be1a2eec22fbfd2f6fc3
Jasan-Aj <mjasan9@gmail.com>
2026-02-14 00:58:49 +0530

    created build

--- back-end/app.js
+++ back-end/app.js
@@ -14,9 +14,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.static('dist'));
-
-app.use(cors());
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
