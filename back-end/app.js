73309163e5ad2056b9f0441c36e8bb18ee3ef45a
Jasan-Aj <mjasan9@gmail.com>
2026-02-13 20:30:55 +0530

    set up vercel.json file

--- back-end/app.js
+++ back-end/app.js
@@ -37,11 +37,11 @@ app.get("/",(req, res)=>{
 });
 
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
-}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
 
 export default app;
