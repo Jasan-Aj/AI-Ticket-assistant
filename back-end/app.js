3e0ae86986281ec94fc71a223835e6403257cbd2
Jasan-Aj <mjasan9@gmail.com>
2026-02-13 23:40:25 +0530

    minor changes

--- back-end/app.js
+++ back-end/app.js
@@ -16,7 +16,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://ai-ticket-assistant-4qfw.vercel.app',
+}));
 
 //   credentials: true,
 //   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
